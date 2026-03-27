a1b59a9eca124b5fa52c4135053cedba6df38812
IlonaChupa <ilonachupa@gmail.com>
2026-03-27 11:23:48 +0200

    done

--- src/test/java/core/basesyntax/service/impl/DataConverterImplTest.java
+++ src/test/java/core/basesyntax/service/impl/DataConverterImplTest.java
@@ -3,7 +3,6 @@ package core.basesyntax.service.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.DataConverter;
 import java.util.ArrayList;
--- src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
+++ src/test/java/core/basesyntax/service/impl/ReportGeneratorImplTest.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service.impl;
 
 import core.basesyntax.db.Storage;
-
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
